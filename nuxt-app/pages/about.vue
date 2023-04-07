3c01efc56b736ca06f95c86d0542ff091d341684
Beneilton Martins Leite <120138604+Beneilton-Martins@users.noreply.github.com>
2023-04-07 14:37:36 -0300

    add- content to pages

---
+++ nuxt-app/pages/about.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div>
+        <p>About</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div>
+        <p>Home</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
