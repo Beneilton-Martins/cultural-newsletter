e1d51c675e49508dc2249304942fb4d22c1a79d3
Beneilton Martins Leite <120138604+Beneilton-Martins@users.noreply.github.com>
2023-04-28 13:21:48 -0300

    add-- text test

--- nuxt-app/components/MainSection.vue
+++ nuxt-app/components/MainSection.vue
@@ -5,6 +5,43 @@
         <p>Bem-vindo ao meu site!</p>
         <button class="cta-button">Saiba mais</button>
     </div>
+    <div class="hero-section">
+        <p>
+            Trabalhar para a gringa no Brasil é perfeito quando se trata de impostos.
+
+            Vem que eu te ensino como pagar pouquíssimo imposto e ganhar como um juíz federal.
+
+            O Brasil incentiva a exportação, isso significa que sua empresa deixa (legalmente) de pagar uma série de
+            impostos como IPI, ICMS, Cofins, PIS, ISS.
+
+            Quando você trabalha para fora, a melhor opção é ser PJ (não pode ser MEI).
+
+            Pode parecer trabalhoso abrir uma empresa, mas não é, e compensa MUITO.
+
+            Quando você abre uma empresa você precisa de um contador que você paga mensalmente, e você vai poder trocar
+            idéia com ele.
+
+            Aqui tá o pulo do gato para você economizar uma grana enorme:
+
+            Ao abrir uma empresa você pode mandar a seguinte mensagem para ele:
+
+            "Vou ganhar X recorrentemente e quero saber se para o meu caso em específico seria melhor bolar uma estratégia
+            no simples nacional ou no lucro presumido, me apresente o melhor cenário."
+
+            A frase acima vai te economizar um dinheiro astronômico, de nada.
+
+            No fim das contas você vai pagar uns 6% de impostos mensais (100% legal com o leão).
+
+            Comparados aos 50% de alguns lugares da Europa, isso é um paraíso.
+
+            Com a economia que você fizer dos impostos dá pra você contribuir com a sociedade fazendo doações diretamente
+            aos pobres (ao invés de pagar lagosta pra político).
+
+            Se você quer ficar rico com programação essa é a melhor opção. Dá pra viver como um rei no Brasil.
+
+            Principalmente se você focar nos estados unidos, que tem os melhores salários.
+        </p>
+    </div>
 </template>
   
 <style scoped>
@@ -14,19 +51,6 @@
     justify-content: center;
     align-items: center;
     margin-top: 32px;
-}
-
-.hero-section h1 {
-    font-size: 36px;
-    font-weight: bold;
-    text-align: center;
-    margin-bottom: 16px;
-}
-
-.hero-section p {
-    font-size: 24px;
-    text-align: center;
-    margin-bottom: 32px;
 }
 
 .cta-button {
