af309e8875a0b6587caef8abbd43f71671770e39
Beneilton Martins Leite <120138604+Beneilton-Martins@users.noreply.github.com>
2023-04-29 13:28:57 -0300

    more

--- nuxt-app/components/MainSection.vue
+++ nuxt-app/components/MainSection.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="hero-section">
         <h1> Cyperus Papys </h1>
-        <p> Cyper, Yrus e a miopia dos desinformados </p>
+        <p> Cyper, o faro dos curiosos </p>
+        <p> Yrus, a miopia dos desinformados </p>
         <button> Desmiopitizesse </button>
         <img src="../public/mole.jpg" alt="topeira">
         <h2> Projeto em Construção </h2>
         <p> Tenha Paciência </p>
+        <p> Tudo nesse tela é apenas experimental </p>
 
     </div>
 </template>
