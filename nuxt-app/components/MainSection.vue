95a198b40463f84f89314c8b0baf3ed2a9d564c6
Beneilton Martins Leite <120138604+Beneilton-Martins@users.noreply.github.com>
2023-04-29 13:21:50 -0300

    topeira img

--- nuxt-app/components/MainSection.vue
+++ nuxt-app/components/MainSection.vue
@@ -3,6 +3,10 @@
         <h1> Cyperus Papys </h1>
         <p> Cyper, Yrus e a miopia dos desinformados </p>
         <button> Desmiopitizesse </button>
+        <img src="../public/mole.jpg" alt="topeira">
+        <h2> Projeto em Construção </h2>
+        <p> Tenha Paciência </p>
+
     </div>
 </template>
   
@@ -15,4 +19,9 @@
     margin: 32px;
     gap: 10px;
 }
+
+img {
+    height: 300px;
+}
+
 </style> 
